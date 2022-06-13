fcfb7bc457b8b759a3f6ce8b36986c50f8784fdc
Leonardo <fanger53@gmail.com>
2022-06-13 10:22:59 -0500

    add test for hash_json

--- spec/hash_json_spec.rb
+++ spec/hash_json_spec.rb
@@ -1,10 +1,17 @@
 require './hash_json'
 require 'json'
 
-describe "sort_by_price_ascending" do
+input = '[{"name":"eggs","price":10},{"name":"coffee","price":9.99},{"name":"rice","price":4.04}]'
+
+input2 = '[{"name":"eggs","price":10},{"name":"coffee","price":9.99},{"name":"rice","price":10}]'
+
+describe "use of json in ruby" do
   it 'order items by price ascending order' do
-    input = '[{"name":"eggs","price":10},{"name":"coffee","price":9.99},{"name":"rice","price":4.04}]'
     expect(sort_by_price_ascending(input)).to eq '[{"name":"rice","price":4.04},{"name":"coffee","price":9.99},{"name":"eggs","price":10}]'
+  end
+
+  it 'two items with same price order by name' do
+    expect(sort_by_price_ascending(input2)).to eq '[{"name":"coffee","price":9.99},{"name":"eggs","price":10},{"name":"rice","price":10}]'
   end
 end
 
